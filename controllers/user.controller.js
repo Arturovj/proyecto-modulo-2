9992715a0e552ccc1b434c0e30bbd2ac2554e067
Sergio Villabrille <sergiovillabrille@hotmail.com>
2022-03-02 21:39:01 +0100

    profile Cards

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,16 @@
+const { find } = require("../models/gym.model")
+const User = require("../models/user.model")
+const Comment = require("../models/comment.model")
 
-module.exports.profile = (req, res, next) =>{
-    res.render('users/profile')
+module.exports.profile = (req, res, next) => {
+    Comment.find({user: req.user.id})
+    .populate('gym')
+    .then((comments) => {
+        const gyms = comments.map((comment) => comment.gym);
+        const cleanGyms = [];
+        gyms.forEach((gym) => !cleanGyms.some(g => g.id === gym.id) && cleanGyms.push(gym))
+
+        console.log(cleanGyms)
+        res.render('users/profile', { gyms: cleanGyms })
+    })
 }
