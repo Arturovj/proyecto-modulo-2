500c3d9d57bc36c8185df2c609104a35b4367ab2
Sergio Villabrille <sergiovillabrille@hotmail.com>
2022-03-06 22:01:57 +0100

    Profile done with cloudinary aand edit

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,20 +9,45 @@ module.exports.profile = (req, res, next) => {
         const gyms = comments.map((comment) => comment.gym);
         const cleanGyms = [];
         gyms.forEach((gym) => !cleanGyms.some(g => g.id === gym.id) && cleanGyms.push(gym))
-
-        console.log(cleanGyms)
-        res.render('users/profile', { gyms: cleanGyms })
+        res.render('partials/profile', { gyms: cleanGyms })
     })
 }
 
-module.exports.edit = (req, res, next) => {
-
+module.exports.editUser = (req, res, next) => {
+    console.log(req.params.id)
     User.findById(req.params.id)
       .then((user) => {
-        res.render('users/edit', {
+        res.render('users/editUser', {
           user,
         });
       })
       .catch(next)
   
+};
+
+module.exports.doEditUser = (req, res, next) => {
+    console.log(req.body)
+    if (req.file){
+        req.body.image = req.file.path
+    } else {
+        delete req.body.image
+    }
+
+    User.findByIdAndUpdate(req.params.id, req.body, { runValidators: true, new: true })
+      .then((user) => {
+          console.log(user)
+          res.redirect('/profile')
+        })
+      .catch((error) => {
+        if (error instanceof mongoose.Error.ValidationError) {
+          req.body.id = req.params.id;
+          res.status(400).render(`users/${user._id}/edit`, {
+            errors: error.errors,
+            user: req.body,
+            categories: categories,
+          });
+        } else {
+          next(error);
+        }
+      });
   };
